a78944d1ecf1285ae61d03f2cf953e9989b2fda4
Amish Patel <amish.patel@digital.cabinet-office.gov.uk>
2023-10-02 15:39:57 +0100

    test: disable line length for tests

--- Tests/NetworkingTests/NetworkClientTests.swift
+++ Tests/NetworkingTests/NetworkClientTests.swift
@@ -76,9 +76,11 @@ extension NetworkClientTests {
         }
         
         let bearerToken = request.value(forHTTPHeaderField: "Authorization")
+        // swiftlint:disable line_length
         let tokenToCheck = """
       Bearer zh2JZEiVWK9AHJNQN3TiK7MFjbuECjyA-I9uL9oFHmNZv0v4HnJrs64ZiNjp9nOSJjAM7gBIWJXlnzxG07/wzSE?neXnLinU6YYAZ!Q2CRwxkeqWT=hBapLWnbP1g/5j2MXlnY7gw2T=?i6nBI86=MJ1NR!fxqlwg4iI73x9HTL15i6urRrICHEps2BEy0c0uAet6USg6dpy0fFnf6Cf!ZozO37TOJB=T7!FooE8HDlM8WXggsO6cDQdDzRS43gL
       """
+        // swiftlint:enable line_length
         
         XCTAssertEqual(bearerToken, tokenToCheck)
     }
