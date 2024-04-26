e0afd870e01f95a3d37424e4f9ba1eb1267f46e3
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-04-26 16:47:04 +0100

    fix: replacing hyphens with underscores

--- Sources/Networking/Authentication/URLRequest+Extensions.swift
+++ Sources/Networking/Authentication/URLRequest+Extensions.swift
@@ -7,10 +7,10 @@ extension URLRequest {
         request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
         var urlParser = URLComponents()
         urlParser.queryItems = [
-            URLQueryItem(name: "grant-type", value: "urn:ietf:params:oauth:grant-type:token-exchange"),
+            URLQueryItem(name: "grant_type", value: "urn:ietf:params:oauth:grant-type:token-exchange"),
             URLQueryItem(name: "scope", value: scope),
-            URLQueryItem(name: "subject-token", value: subjectToken),
-            URLQueryItem(name: "subject-token-type", value: "urn:ietf:params:oauth:token-type:access_token")
+            URLQueryItem(name: "subject_token", value: subjectToken),
+            URLQueryItem(name: "subject_token_type", value: "urn:ietf:params:oauth:token-type:access_token")
         ]
         request.httpBody = urlParser.percentEncodedQuery?.data(using: .utf8)
         return request
