0e8f8d45823d080171a9171c9eb2c4259b9b6420
dylanjames-dd <dylan.james@digital.cabinet-office.gov.uk>
2024-11-21 13:59:36 +0000

    fix: JWTRepresentation public initialiser (#33)
    * fix: jwt representation initialiser public
    
    * fix: linting

--- Sources/TokenGeneration/JWT/JWTRepresentation.swift
+++ Sources/TokenGeneration/JWT/JWTRepresentation.swift
@@ -1,4 +1,9 @@
 public struct JWTRepresentation {
     let header: [String: Any]
     let payload: [String: Any]
+    
+    public init(header: [String: Any], payload: [String: Any]) {
+        self.header = header
+        self.payload = payload
+    }
 }
