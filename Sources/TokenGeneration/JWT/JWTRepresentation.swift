798a1617adcb24a0ed932d779e79f4c27b7ff6d4
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-11-21 13:55:39 +0000

    fix: linting

--- Sources/TokenGeneration/JWT/JWTRepresentation.swift
+++ Sources/TokenGeneration/JWT/JWTRepresentation.swift
@@ -2,7 +2,7 @@ public struct JWTRepresentation {
     let header: [String: Any]
     let payload: [String: Any]
     
-    public init(header: [String : Any], payload: [String : Any]) {
+    public init(header: [String: Any], payload: [String: Any]) {
         self.header = header
         self.payload = payload
     }
