5d251ecc5be47d6e8ff05d06379670eddcb6c49d
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-11-21 11:12:42 +0000

    docs: wording change

--- Sources/TokenGeneration/Extensions/Data+Extensions.swift
+++ Sources/TokenGeneration/Extensions/Data+Extensions.swift
@@ -4,10 +4,10 @@ extension Data {
     /// Base 64 encoding the string.
     /// Description of this format: https://datatracker.ietf.org/doc/html/rfc4648#section-5
     ///
-    /// Removing unallowed URL encoded character:
+    /// Removing unsupported URL encoded character:
     ///   - "="
     ///
-    /// Replacing unallowed URL encoded characters:
+    /// Replacing unsupported URL encoded characters:
     ///   - "+" with "-"
     ///   - "/" with "_"
     var base64URLEncodedString: String {
