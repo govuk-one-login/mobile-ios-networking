f6f7b19d2355254ceb3b6c2e4f759efd1f4cfe4f
Dylan James <dylan.james@digital.cabinet-office.gov.uk>
2024-12-05 18:13:09 +0000

    test: changing implementation

--- Sources/TokenGeneration/JWT/JWTGenerator.swift
+++ Sources/TokenGeneration/JWT/JWTGenerator.swift
@@ -17,17 +17,13 @@ public struct JWTGenerator {
                 throw JWTGeneratorError.cantCreateJSONData
             }
             
-            guard let headerJSONString = String(data: headerData, encoding: .utf8),
-                  let payloadJSONString = String(data: payloadData, encoding: .utf8) else {
-                throw JWTGeneratorError.cantCreateJSONString
-            }
-            
-            let signableJSONString = headerJSONString + "." + payloadJSONString
-            let dataToSign = Data(signableJSONString.utf8)
-            let signature = try signingService.sign(data: dataToSign)
-            
             let encodedHeader = headerData.base64URLEncodedString
             let encodedPayload = payloadData.base64URLEncodedString
+            
+            let signableJSONString = encodedHeader + "." + encodedPayload
+            let dataToSign = Data(signableJSONString.utf8)
+            let signature = try signingService.sign(data: dataToSign)
+
             let encodedSignature = signature.base64URLEncodedString
             
             return "\(encodedHeader).\(encodedPayload).\(encodedSignature)"
